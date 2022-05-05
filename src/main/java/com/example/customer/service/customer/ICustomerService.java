ae70d78228c5eda88aadec0f71a5513988601cdb
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2022-05-05 23:06:32 +0700

    hieu luong

---
+++ src/main/java/com/example/customer/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.customer.service.customer;
+
+import com.example.customer.model.Customer;
+import com.example.customer.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
